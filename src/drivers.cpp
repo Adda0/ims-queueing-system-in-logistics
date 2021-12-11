326b7ab796e59fa830c3072cdfe4cf6963f6045e
dejv.mihola <david.mihola@sperling.cz>
2021-12-11 13:59:08 +0100

    driver load stats with -f option

--- src/drivers.cpp
+++ src/drivers.cpp
@@ -10,3 +10,26 @@
 const Store * const Cars::cars = &Cars::_store;
 const Store * const Bikes::bikes = &Bikes::_store;
 
+bool DriverLoad::printCSV(false);
+
+void DriverLoad::Behavior()
+{
+    _driverLoad.push_back( 
+    { 
+        .time = static_cast<unsigned>(Time), 
+        .carLoad = static_cast<unsigned>(Cars::cars->Used()), 
+        .bikeLoad = static_cast<unsigned>(Bikes::bikes->Used()) 
+    });
+
+    Activate(Time + 1);
+}
+
+void DriverLoad::PrintCSV()
+{
+    cout << "time,cars,bikes" << endl;
+    for (auto line : _driverLoad)
+    {
+        cout << line.time << "," << line.carLoad << "," << line.bikeLoad << endl;
+    }
+}
+
